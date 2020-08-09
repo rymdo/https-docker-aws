e80fa7080eddd93b509377a68609864e63e911c7
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-08-09 18:00:28 +0200

    feat: add subnet and internet gateway to aws vpc

--- infrastructure/aws-modules/modules/vpc/main.tf
+++ infrastructure/aws-modules/modules/vpc/main.tf
@@ -1,8 +1,47 @@
-resource "aws_vpc" "primary" {
+resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
+    Name        = var.environment
     Environment = var.environment
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_subnet
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.availability_zone
+  tags = {
+    Name        = "${var.environment}-public"
+    Environment = var.environment
+  }
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.environment}-public"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.environment}-public"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.public.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
